65918652ed492b8e7305194cf3052bc5ddff9cd4
Murilo Augusto <murilo.fer22@gmail.com>
2026-04-14 01:48:42 -0300

    feat_TryCatch_InvariantCulture

---
+++ 04_TryCatch_InvariantCulture/Customer.cs
@@ -0,0 +1,48 @@
+﻿using System.Globalization;
+namespace _04_TryCatch_InvariantCulture
+{
+    internal class Customer
+    {
+        public string Name;
+
+        private double _accountBalance;
+        public double AccountBalance
+        {
+            get { return _accountBalance; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("""
+                        The account balance can not be under 0 dollars.
+                        This bank does not offer loans.
+                        
+                        """);
+                }
+                _accountBalance = value;
+            }
+        }
+
+        private int _age;
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value < 16)
+                {
+                    throw new ArgumentException("customers must be at least 16 years old");
+                }
+                _age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format(CultureInfo.InvariantCulture,
+                "Name: {0} // Age: {1} // Account Balance: ${2:N2}",
+                Name, Age, AccountBalance);
+        }
+    }
+}
+
---
+++ 04_TryCatch_InvariantCulture/Program.cs
@@ -0,0 +1,54 @@
+﻿using _04_TryCatch_InvariantCulture;
+using System.Globalization;
+
+Customer myCustomer = new Customer();
+
+Console.Write("Enter the customer name: ");
+myCustomer.Name = (Console.ReadLine());
+
+bool validAnswer1 = false;
+while (!validAnswer1)
+{
+    try
+    {
+        Console.Write("Enter the customer age: ");
+        myCustomer.Age = int.Parse(Console.ReadLine());
+        validAnswer1 = true;
+    }
+    catch (ArgumentException ex)
+    {
+        Console.WriteLine($"invalid input. {ex.Message}");
+        Console.WriteLine("Please, Try again.");
+    }
+    catch (FormatException ex)
+    {
+        Console.WriteLine("Error. You must enter only numbers");
+        Console.WriteLine("Please, Try again.");
+    }
+}
+
+bool validAnswer2 = false;
+while (!validAnswer2)
+{
+    try
+    {
+        Console.Write("Enter the customer account balance: ");
+        myCustomer.AccountBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        validAnswer2 = true;
+    }
+    catch (ArgumentException ex)
+    {
+        Console.WriteLine($"invalid input. {ex.Message}");
+        Console.WriteLine("Please, Try again.");
+    }
+    catch (FormatException ex)
+    {
+        Console.WriteLine("Error. You must enter only numbers");
+        Console.WriteLine("Please, Try again.");
+    }
+}
+
+Console.WriteLine($@"
+----------------CUSTOMER DATA----------------
+{myCustomer}"
+);
