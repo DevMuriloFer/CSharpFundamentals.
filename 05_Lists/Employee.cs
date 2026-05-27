3084c56f7c22d110876e128c088fb1a383155754
Murilo Augusto <murilo.fer22@gmail.com>
2026-05-27 17:40:48 -0300

    New Exercise done

---
+++ 05_Lists/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05_Lists
+{
+    internal class Employee
+    {
+        public int Id;
+        public string Name;
+        public double Salary;
+
+        public void increaseSalary(double percentage) 
+        {
+            Salary += Salary * (percentage / 100);
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {Id, -6}| Name: {Name, -30}| Salary: {Salary, -12:F2}";
+        }
+    }
+}
---
+++ 05_Lists/Program.cs
@@ -0,0 +1,54 @@
+﻿using _05_Lists;
+
+List<Employee> employees = new List<Employee>();
+int newEmployeesCount;
+
+int idToIncreaseSalary;
+int indexToIncreaseSalary;
+
+double percentage;
+
+//employee registration
+Console.Write("How many employees will be registered?: ");
+newEmployeesCount = int.Parse(Console.ReadLine());
+
+for (int i =0; i < newEmployeesCount; i++)
+{
+    Console.Write(
+        $"""
+
+        Employee #{i + 1}:
+        ID: 
+        """);
+    int id = int.Parse(Console.ReadLine()); 
+    Console.Write("Name: ");
+    string name = Console.ReadLine();
+    Console.Write("Salary: ");
+    double salary  = double.Parse(Console.ReadLine());
+
+    employees.Add(new Employee { Id = id, Name = name, Salary = salary});
+}
+
+//Salary increase
+Console.Write("\nEnter the employee id that will have salary increase: ");
+idToIncreaseSalary = int.Parse(Console.ReadLine());
+
+indexToIncreaseSalary = employees.FindIndex(X => X.Id == idToIncreaseSalary);
+if (indexToIncreaseSalary == -1)
+{
+    Console.WriteLine("This id does not exist!");
+}
+else
+{
+    Console.Write("Enter the percentage: ");
+    percentage = double.Parse(Console.ReadLine());
+
+    employees[indexToIncreaseSalary].increaseSalary(percentage);
+}
+
+//Displaying the updated employee list
+Console.WriteLine("\nUpdated list of employees: ");
+for(int i = 0; i < newEmployeesCount; i++)
+{
+    Console.WriteLine(employees[i]);
+}
