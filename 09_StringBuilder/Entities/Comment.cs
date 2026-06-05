19ef1afb7cbb3e99a0cd483b15165767578cba1d
Murilo Augusto <murilo.fer22@gmail.com>
2026-06-05 00:14:33 -0300

    New Exercise: 09_StringBuilder

---
+++ 09_StringBuilder/Entities/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09_StringBuilder.Entities
+{
+    internal class Comment
+    {
+        public string Text { get; set; }
+        
+        public Comment()
+        {
+        }
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
---
+++ 09_StringBuilder/Entities/Post.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace _09_StringBuilder.Entities
+{
+    internal class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        Post()
+        {
+        }
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.Append(Likes);
+            sb.Append(" Likes - ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Comments:");
+            for (int i = 0; i < Comments.Count; i++) 
+            {
+                sb.AppendLine(Comments[i].Text);
+            }
+            return sb.ToString();
+        }
+    }
+}
